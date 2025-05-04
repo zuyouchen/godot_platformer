09ca49d9cc5cbfe9da7e495426e82516337548b8
Evan Chen <echen48@illinois.edu>
2025-05-04 18:59:46 -0500

    ensure score reset

--- scripts/level_complete.gd
+++ scripts/level_complete.gd
@@ -9,4 +9,5 @@ func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
 func _on_restart_button_pressed() -> void:
+	GlobalVars.score = 0
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
